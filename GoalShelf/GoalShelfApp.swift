e7e844b0325bcf16051adffdf315332185bd06b2
Snesnopic <snesnopic@gmail.com>
2023-10-19 11:01:56 +0200

    Initial Commit

---
+++ GoalShelf/GoalShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalShelfApp.swift
+//  GoalShelf
+//
+//  Created by Giuseppe Francione on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GoalShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
