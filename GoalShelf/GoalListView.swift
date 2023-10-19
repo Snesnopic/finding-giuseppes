afb51544cc7f361c6dfd2e6cb6e7b4264d7f9de5
Snesnopic <snesnopic@gmail.com>
2023-10-19 14:56:49 +0200

    First skeleton of the goals view

---
+++ GoalShelf/GoalListView.swift
@@ -0,0 +1,18 @@
+//
+//  GoalListView.swift
+//  GoalShelf
+//
+//  Created by Giuseppe Francione on 19/10/23.
+//
+
+import SwiftUI
+
+struct GoalListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GoalListView()
+}
