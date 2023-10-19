77479782c952d5036ee2670d690649ba41c78afe
Snesnopic <snesnopic@gmail.com>
2023-10-19 14:57:40 +0200

    First skeleton of the goals view

--- GoalShelf/GoalListView.swift
+++ GoalShelf/GoalListView.swift
@@ -9,7 +9,62 @@ import SwiftUI
 
 struct GoalListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       
+        NavigationStack {
+            Spacer()
+            VStack
+                {
+                    ZStack{
+                        
+                        List()
+                        {
+                            Text("Task").swipeActions {
+                                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                                    /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+                                })
+                            }
+                        }.background(.red.opacity(0.6))
+                            .scrollContentBackground(.hidden)
+                        
+                    }
+                    ZStack{
+                        Color.blue
+                        List()
+                        {
+                            Text("Task").swipeActions {
+                                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                                    /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+                                })
+                            }
+                        }.background(.blue.opacity(0.6))
+                            .scrollContentBackground(.hidden)
+                    }
+                    ZStack{
+                        
+                        List()
+                        {
+                            Text("Task").swipeActions {
+                                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                                    /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+                                })
+                            }
+                        }.background(.green.opacity(0.6))
+                            .scrollContentBackground(.hidden)
+                    }
+                    ZStack{
+                        
+                        List()
+                        {
+                            Text("Task").swipeActions {
+                                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                                    /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+                                })
+                            }
+                        }.background(.yellow.opacity(0.6))
+                            .scrollContentBackground(.hidden)
+                    }
+            }.navigationTitle("Goals")
+        }
     }
 }
 
