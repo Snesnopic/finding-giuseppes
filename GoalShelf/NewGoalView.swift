49afd95892df6d8724262c10b28314d39b314ff1
Giuseppe Casillo <g.casillox2@gmail.com>
2023-10-23 11:25:12 +0200

    added the GoalView (mainView) and the NewModelView (modal)

---
+++ GoalShelf/NewGoalView.swift
@@ -0,0 +1,18 @@
+//
+//  NewGoalView.swift
+//  GoalShelf
+//
+//  Created by Giuseppe Casillo on 23/10/23.
+//
+
+import SwiftUI
+
+struct NewGoalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewGoalView()
+}
