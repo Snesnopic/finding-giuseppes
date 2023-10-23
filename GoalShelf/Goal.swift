58c0a2db29b68d6daf9b323d6e85608e0fdd5d10
Giuseppe Casillo <g.casillox2@gmail.com>
2023-10-23 11:23:07 +0200

    Added Task and Goal classes + GoalViewModel (not completed)

---
+++ GoalShelf/Goal.swift
@@ -0,0 +1,8 @@
+//
+//  Goal.swift
+//  GoalShelf
+//
+//  Created by Giuseppe Casillo on 23/10/23.
+//
+
+import Foundation
---
+++ GoalShelf/Task.swift
@@ -0,0 +1,8 @@
+//
+//  Task.swift
+//  GoalShelf
+//
+//  Created by Giuseppe Casillo on 23/10/23.
+//
+
+import Foundation
