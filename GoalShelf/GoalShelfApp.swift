3fc6c4548476d2ac6e60194a235fbe10b001adb5
Giuseppe Casillo <g.casillox2@gmail.com>
2023-10-27 09:49:07 +0200

    Deleted tab bar

--- GoalShelf/GoalShelfApp.swift
+++ GoalShelf/GoalShelfApp.swift
@@ -29,21 +29,7 @@ struct GoalShelfApp: App {
      
     var body: some Scene {
         WindowGroup {
-            TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
-                GoalView().tabItem {
-                    VStack
-                    {
-                        Image(systemName: "flag.fill")
-                        Text("Goals")
-                    } }.tag(1)
-                GoalListView().tabItem {
-                    VStack{
-                        Image(systemName: "person.3.fill")
-                        Text("Teams")
-                    }
-                     }.tag(2)
-            }
-            
+            GoalView()
         }
         .modelContainer(modelContainer)
         
