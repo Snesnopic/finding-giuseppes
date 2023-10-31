542aac39af0b3dab1b66c3dc74aa9261e110d3a7
Giuseppe Casillo <g.casillox2@gmail.com>
2023-10-31 21:59:29 +0100

    Last touches and function. The TaskDetailView is SHIT, but it works (idk how to set a fkin forEach because Set<weekdayenum> is different from weekdayenum and it says I cannot compare them in the .contains).

---
+++ GoalShelf/View/TaskDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskDetailView.swift
+//  GoalShelf
+//
+//  Created by Giuseppe Casillo on 31/10/23.
+//
+
+import SwiftUI
+
+struct TaskDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskDetailView()
+}
